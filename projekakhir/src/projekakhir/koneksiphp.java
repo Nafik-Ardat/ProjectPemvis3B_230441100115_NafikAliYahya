16b6aa32ffc72088e182c528687bf2c644c05675
Nafik-Ardat <nafikadventur@gmail.com>
2024-11-16 07:10:08 +0700

    230441100115-NafikAliYahya

---
+++ projekakhir/src/projekakhir/koneksiphp.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projekakhir;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author I MASTER ZEN
+ */
+public class koneksiphp {
+    private static final String URL = "jdbc:mysql://localhost:3307/inventoridapur";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    
+    private static Connection conn = null;
+
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USER, PASS);
+                System.out.println("terkoneksi");
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Koneksi Gagal! " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                System.out.println("error bang : " + e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+
+
+    public static void main(String[] args) {
+        getConnection();    
+    }
+}
